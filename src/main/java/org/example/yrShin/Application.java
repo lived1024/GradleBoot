16b89dd8923d80c2d7b4c1def6d4f414f26fa325
lived1024 <lived1024@gmail.com>
2021-05-04 23:11:21 +0900

    등록일, 수정일 자동 처리 추가 및 테스트

--- src/main/java/org/example/yrShin/Application.java
+++ src/main/java/org/example/yrShin/Application.java
@@ -2,8 +2,10 @@ package org.example.yrShin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@EnableJpaAuditing                                          // JPA Auditing 활성화
+@SpringBootApplication                                      // 스프링부트 메인
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
