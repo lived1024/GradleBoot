384c0f2e5c705825c448ec4d34ff28e199f23654
lived1024 <lived1024@gmail.com>
2021-05-05 16:35:34 +0900

    mustache 기초 설정 및 테스트

---
+++ src/main/java/org/example/yrShin/web/IndexController.java
@@ -0,0 +1,16 @@
+package org.example.yrShin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    // mustache의 기본 경로 및 확장자는 자동으로 지정해준다.
+    // 경로   : src/main/resources/templates
+    // 확장자 : .mustache
+    @GetMapping("/")
+    public String index(){
+        return "index";     // src/main/resources/templates/index.mustache      파일을 연결시켜준다
+    }
+}
