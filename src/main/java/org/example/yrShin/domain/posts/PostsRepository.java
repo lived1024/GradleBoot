7985915504432f74fcebdc9c0568923bf407d736
lived1024 <lived1024@gmail.com>
2021-05-07 22:56:02 +0900

    전체 조회 화면 추가

--- src/main/java/org/example/yrShin/domain/posts/PostsRepository.java
+++ src/main/java/org/example/yrShin/domain/posts/PostsRepository.java
@@ -1,6 +1,17 @@
 package org.example.yrShin.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    // jpa를 이용하기 어려운 경우에는 Querydsl, jooq, MyBatis를 이용
+    // 이 책의 저자는 Querydsl을 추천
+    // 1. 타입 안정성 보장 - 오타나 존재하지 않는 컬럼명을 명시할 경우 IDE에서 자동으로 검출 - MyBatis는 지원 X
+    // 2. 쿠팡, 배민 등 JPA를 적극적으로 사용하는 회사에서 Querydsl을 적극적으로 사용중
+    // 3. 레퍼런스가 많음 - 국내 자료가 많음
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/org/example/yrShin/web/IndexController.java
+++ src/main/java/org/example/yrShin/web/IndexController.java
@@ -1,16 +1,27 @@
 package org.example.yrShin.web;
 
+import lombok.RequiredArgsConstructor;
+import org.example.yrShin.service.posts.PostsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
 
     // mustache의 기본 경로 및 확장자는 자동으로 지정해준다.
     // 경로   : src/main/resources/templates
     // 확장자 : .mustache
+//    @GetMapping("/")
+//    public String index(){
+//        return "index";     // src/main/resources/templates/index.mustache      파일을 연결시켜준다
+//    }
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";     // src/main/resources/templates/index.mustache      파일을 연결시켜준다
     }
 
