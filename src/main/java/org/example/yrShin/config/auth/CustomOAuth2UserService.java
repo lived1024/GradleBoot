6523ff36efb1b976585742e7f3f1a642a6ab1d9a
lived1024 <lived1024@gmail.com>
2021-05-14 00:32:09 +0900

    해당 기능의 코드에 설명 추가

--- src/main/java/org/example/yrShin/config/auth/CustomOAuth2UserService.java
+++ src/main/java/org/example/yrShin/config/auth/CustomOAuth2UserService.java
@@ -29,13 +29,18 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
+        // 현재 로그인 진행 중인 서비스를 구분하는 코드 - 구글, 네이버 등 여러 서비스의 로그인 연동 시 구분하기 위해 사용
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        // OAuth2 로그인 진행 시 키가 되는 필드값(PK) - 구글, 네이버 등 여러 서비스의 로그인 지원 시 사용
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
 
+        // OAuth2UserService를 통해 가져온 OAuth2User의 attribute를 담을 클래스 - 이후 다른 소셜 로그인도 이 클래스 사용
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
         User user = saveOrUpdate(attributes);
 
+        // SessionUser : 세션용 DTO 클래스 - 상세 이유는 해당 클래스에서 확인 가능
         httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(
@@ -50,6 +55,6 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                 .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
                 .orElse(attributes.toEntity());
 
-        return userRepository.save(user);
+        return userRepository.save(user);   // save 함수는 UserRepository에서 JpaRepository를 상속하면 쓸 수 있다.
     }
 }
--- src/main/java/org/example/yrShin/config/auth/dto/SessionUser.java
+++ src/main/java/org/example/yrShin/config/auth/dto/SessionUser.java
@@ -5,6 +5,12 @@ import org.example.yrShin.domain.user.User;
 
 import java.io.Serializable;
 
+/* SessionUser : 세션용 DTO 클래스 - 인증된 사용자 정보만 필요 - 직렬화 기능을 가진 세션용 Dto
+ * User 클래스와 별도로 사용하는 이유
+ * 1. 동일하게 사용 시 직렬화를 구현하지 않았다는 에러 발성
+ *     -> User 클래스에 직렬화 구현 시 다른 엔티티를 자식 엔티티로 가진다면 자식 엔티티도 직렬화 대상이 된다.
+ *     -> 성능 이슈, 부수 효과가 발생할 확률이 높음
+ */
 @Getter
 public class SessionUser implements Serializable {
 
