cbb8adf87c4add7af0d2b7a72565983dd09c2420
lived1024 <lived1024@gmail.com>
2021-05-15 00:32:16 +0900

    OAuth2 구글 로그인 연동 코드 추가

--- src/main/java/org/example/yrShin/web/IndexController.java
+++ src/main/java/org/example/yrShin/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.yrShin.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.yrShin.config.auth.dto.SessionUser;
 import org.example.yrShin.service.posts.PostsService;
 import org.example.yrShin.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     // mustache의 기본 경로 및 확장자는 자동으로 지정해준다.
     // 경로   : src/main/resources/templates
@@ -24,6 +28,12 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";     // src/main/resources/templates/index.mustache      파일을 연결시켜준다
     }
 
