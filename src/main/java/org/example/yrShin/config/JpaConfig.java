21032b4b95fef9c9e644cb1f80325f0561b3de85
lived1024 <lived1024@gmail.com>
2021-05-19 23:05:17 +0900

    스프링 시큐리티 적용 후 테스트코드 수정 -> 현재 에러 발생되는 테스트 2개 남아있음

---
+++ src/main/java/org/example/yrShin/config/JpaConfig.java
@@ -0,0 +1,7 @@
+package org.example.yrShin.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing      // JPA Auditing 활성화
+public class JpaConfig {
+}
